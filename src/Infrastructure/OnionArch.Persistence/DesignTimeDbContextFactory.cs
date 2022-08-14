be5a89d2c4c2bcb532a01b59c50eb76472b9e99d
Furkan Aydin <furknn2112@gmail.com>
2022-08-14 16:38:24 +0300

    Implementation of IDesignTimeDbContextFactory

---
+++ src/Infrastructure/OnionArch.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using OnionArch.Persistence.Contexts.MsSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceDbContext>
+    {
+        public ECommerceDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ECommerceDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ECommerceGY;Trusted_Connection=True;");
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
