f46a13d0f360b5e7571764fd05b72817f8acb654
Furkan Aydin <furknn2112@gmail.com>
2022-08-16 15:03:14 +0300

    Repository Read ve Write için concrete nesneleri oluşturuldu.

---
+++ src/Infrastructure/OnionArch.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArch.Application.Repositories;
+using OnionArch.Domain.Entities.Common;
+using OnionArch.Persistence.Contexts.MsSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Persistence.Repositories
+{
+    internal class ReadRepository<TEntity> : IReadRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly ECommerceDbContext _dbContext;
+
+        public ReadRepository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public DbSet<TEntity> Table => _dbContext.Set<TEntity>();
+
+        public IQueryable<TEntity> GetAll() 
+            => Table.AsQueryable();
+
+        public IQueryable<TEntity> GetAllByCondition(Expression<Func<TEntity, bool>> filter)
+            => Table.Where(filter);
+
+        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> filter)
+            => await Table.FirstOrDefaultAsync(filter);
+
+        public Task<TEntity> GetByIdAsync(string id)
+            => Table.FirstOrDefaultAsync(entity => entity.Id == Guid.Parse(id));        
+    }
+}
---
+++ src/Infrastructure/OnionArch.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using OnionArch.Application.Repositories;
+using OnionArch.Domain.Entities.Common;
+using OnionArch.Persistence.Contexts.MsSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Persistence.Repositories
+{
+    public class WriteRepository<TEntity> : IWriteRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly ECommerceDbContext _dbContext;
+
+        public WriteRepository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public DbSet<TEntity> Table
+            => _dbContext.Set<TEntity>();
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry = await Table.AddAsync(entity);
+            bool IsAdd = entityEntry.State == EntityState.Added ? true : false;
+            return IsAdd;
+        }
+
+        public async Task<bool> AddRangeAsync(List<TEntity> entities)
+        {
+            await Table.AddRangeAsync(entities);
+            return true;
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry =  Table.Remove(entity);
+            bool IsRemove = entityEntry.State.Equals(EntityState.Deleted);
+            return IsRemove;
+        }
+
+        public bool RemoveRange(List<TEntity> entities)
+        {
+            Table.RemoveRange(entities);
+            return true;
+        }
+
+        public async Task<bool> RemoveByIdAsync(string id)
+        {
+            TEntity entity = await Table.FirstOrDefaultAsync(entity => entity.Id == Guid.Parse(id));
+            bool IsRemove =  Remove(entity);
+            return IsRemove;
+        }
+
+        public bool Update(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry = Table.Update(entity);
+            bool IsUpdate = entityEntry.State.Equals(EntityState.Modified);
+            return IsUpdate;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            int result = await _dbContext.SaveChangesAsync();
+            return result;
+        }
+    }
+}
