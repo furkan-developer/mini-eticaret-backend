72d94bb094e3dc21e168b94b6c3229ccb639654e
Furkan Aydin <furknn2112@gmail.com>
2022-08-16 14:58:39 +0300

    Özel bir nesne için Read ve Write abstract nesneleri oluşturuldu.

---
+++ src/Core/OnionArch.Application/Repositories/Product/IProductReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnionArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Application.Repositories
+{
+    public interface IProductReadRepository : IReadRepository<Product>
+    {
+    }
+}
